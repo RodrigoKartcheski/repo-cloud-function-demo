94af3764a439d083ba602b0611d67cc99ccf9711
RodrigoKartcheki <rodrigo.kartcheski@gmail.com>
2024-09-11 20:21:31 -0300

    revisao 3

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main 2
 import functions_framework
 import os, json
 from google.cloud import bigquery, storage
@@ -14,10 +13,10 @@ def main_gcs(cloud_event):
     event_id = cloud_event["id"]  # Certifique-se de que o ID está no payload
     event_type = cloud_event["type"]
 
-    sourceRaw = os.environ.get('sourceRaw')
-    fileType = os.environ.get('fileType')
-    dataSet_name = os.environ.get('dataSet_name')
-    tableName = os.environ.get('tableName')
+    sourceRaw = os.environ.get('SOURCE_RAW')
+    fileType = os.environ.get('FILE_TYPE')
+    dataSet_name = os.environ.get('DATASET_ID')
+    tableName = os.environ.get('TABLE_NAME')
 
     # Acessando os atributos do objeto 'data' corretamente
     bucket = data.get("bucket")
@@ -66,3 +65,19 @@ def main_gcs(cloud_event):
     print(f"Metageneration: {metageneration}")
     print(f"Created: {timeCreated}")
     print(f"Updated: {updated}")
+    
+
+'''
+gcloud functions deploy python-finalize-function \
+--gen2 \
+--runtime=python312 \
+--region=us-central1 \
+--source=gs://gcs-to-bigquery-3065/fu/source_function.zip \
+--entry-point=main_gcs \
+--trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
+--trigger-event-filters="bucket=gcs-to-bigquery-3065" \
+--allow-unauthenticated \
+--timeout=120s \
+--max-instances=2 \
+--set-env-vars=SOURCE_RAW=raw/,FILE_TYPE=.csv,DATASET_ID=dataset_demo,TABLE_NAME=table_demo,LOG_EXECUTION_ID=true
+'''
